2492ce9ac5c7f87da495b761eef8e14948c7868e
pale-ale <alex.baucke@tutanota.com>
2023-05-28 19:01:55 +0200

    organize plotting and calculations

---
+++ main.py
@@ -0,0 +1,30 @@
+from regression import regression
+from util import Plotter
+from numpy import vstack
+
+POINT_COUNT = 10
+REGRESSAND_DEGREE = 3
+assert POINT_COUNT >= REGRESSAND_DEGREE
+
+DRAW_PTS = True
+DRAW_STEM = True
+DRAW_REGRESSION = True
+DRAW_ERRORS = True
+
+def main():
+  plotter = Plotter()
+  stem_func = lambda x: 0.5 * x**2
+  pts = plotter.get_datapts(stem_func, POINT_COUNT, 1, seed=0)
+
+  if DRAW_PTS: plotter.plot_pts(*pts, 'o', color=(1,0,1))
+  if DRAW_STEM: plotter.plot_func(stem_func)
+  if DRAW_REGRESSION: plotter.plot_func(regression(pts, REGRESSAND_DEGREE), color=(0,1,0))
+  if DRAW_ERRORS:
+    xx = vstack([pts[0], pts[0]])
+    yy = vstack([pts[1], regression(pts, REGRESSAND_DEGREE)(pts[0])])
+    plotter.plot_pts(xx ,yy, ".-.", color=(1,.5,0))
+  plotter.save('/tmp/regression_pts')
+  plotter.show()
+
+if __name__ == "__main__":
+  main()
--- util.py
+++ util.py
@@ -2,21 +2,48 @@ import numpy as np
 from random import uniform, seed as set_seed
 import matplotlib.pyplot as plt
 
-XMIN, XMAX = 0, 4
+plt.rcParams.update({
+    "figure.facecolor":  (1.0, 0.0, 0.0, 0.0),
+    "axes.facecolor":    (0.0, 1.0, 0.0, 0.0),
+    "savefig.facecolor": (0.0, 0.0, 1.0, 0.0),
+    "axes.edgecolor": (0,1,1,1),
+    "xtick.color": (0,1,1,1),
+    "ytick.color": (0,1,1,1),
+    "lines.color": (1,0,1),
+    "xtick.labelsize": 'large',
+    "ytick.labelsize": 'large',
+})
 
-def get_datapts(func, c:int, n:int, xmin, xmax, errmin, errmax, seed:int|None=None):
-    '''Returns an array with c columns and n rows, i.e. c points of dimension n.'''
-    if isinstance(seed, int):
-        set_seed(seed)
-    datamatrix = np.ndarray((n+1,c))
-    datamatrix[0] = np.linspace(xmin, xmax, c)
-    datamatrix[1] = func(datamatrix[0])
-    ynoise = np.fromiter((uniform(errmin[i], errmax[i]) for i in range(n) for _ in range (c)), float, count=c*n).reshape((n,c))
-    datamatrix[1:,] += ynoise
-    return datamatrix
+class Plotter:
+    def __init__(self, xmin=0, xmax=4, ymin=-1, ymax=5, errmin=None, errmax=None):
+        self.ERRMIN = errmin if errmin is not None else [-1]
+        self.ERRMAX = errmax if errmax is not None else [1]
+        self.XMIN, self.XMAX = xmin, xmax
+        self.YMIN, self.YMAX = ymin, ymax
+        self.axes = plt.axes()
+        self.axes.set_xlim((self.XMIN, self.XMAX))
+        self.axes.set_ylim((self.YMIN, self.YMAX))
+    
+    def plot_pts(self, xs, ys, *args, **kwargs):
+        self.axes.plot(xs, ys, *args, **kwargs)
 
-def plot_func(f,**kwargs):
-    xs = np.linspace(XMIN, XMAX, 1000)
-    ys = f(xs)
-    print(xs, ys)
-    plt.plot(xs, ys, **kwargs)
+    def plot_func(self, f, *args, **kwargs):
+        xs = np.linspace(self.XMIN, self.XMAX, 1000)
+        self.axes.plot(xs, f(xs), *args, **kwargs)
+
+    def get_datapts(self, func, c:int, n:int, seed:int|None=None):
+        '''Returns an array with c columns and n rows, i.e. c points of dimension n.'''
+        if isinstance(seed, int):
+            set_seed(seed)
+        datamatrix = np.ndarray((n+1,c))
+        datamatrix[0] = np.linspace(self.XMIN, self.XMAX, c)
+        datamatrix[1] = func(datamatrix[0])
+        ynoise = np.fromiter((uniform(self.ERRMIN[i], self.ERRMAX[i]) for i in range(n) for _ in range (c)), float, count=c*n).reshape((n,c))
+        datamatrix[1:,] += ynoise
+        return datamatrix
+
+    def show(self):
+        plt.show()
+
+    def save(self, path):
+        plt.savefig(path)
