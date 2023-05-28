3b9158bfcbd82a3eb25285315d6fe0422c7c2289
Gal Lebel <galbalandroid@gmail.com>
2023-05-28 17:44:58 +0200

    added plot_func to plot functions easier with pyplot

--- util.py
+++ util.py
@@ -1,5 +1,8 @@
 import numpy as np
 from random import uniform, seed as set_seed
+import matplotlib.pyplot as plt
+
+XMIN, XMAX = 0, 4
 
 def get_datapts(func, c:int, n:int, xmin, xmax, errmin, errmax, seed:int|None=None):
     '''Returns an array with c columns and n rows, i.e. c points of dimension n.'''
@@ -11,3 +14,9 @@ def get_datapts(func, c:int, n:int, xmin, xmax, errmin, errmax, seed:int|None=No
     ynoise = np.fromiter((uniform(errmin[i], errmax[i]) for i in range(n) for _ in range (c)), float, count=c*n).reshape((n,c))
     datamatrix[1:,] += ynoise
     return datamatrix
+
+def plot_func(f,**kwargs):
+    xs = np.linspace(XMIN, XMAX, 1000)
+    ys = f(xs)
+    print(xs, ys)
+    plt.plot(xs, ys, **kwargs)
