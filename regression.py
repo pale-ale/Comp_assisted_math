92dad66c4a8890b3969704f249639be4d6976258
GaLebel <74780857+GaLebel@users.noreply.github.com>
2023-05-30 19:27:36 +0200

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,8 +1,7 @@
 from sage.all import *
-import numpy as np
 
 def regression(points, dim:int=2):
-    # Ax = b -> canot be solved for x. But:
+    # Ax = b -> cannot be solved for x. But:
     # A^tAx = A^tb represents a projection into solution space, which has a solution, so:
     # x = (A^tA)^-1 * A^tb
 
@@ -11,30 +10,30 @@ def regression(points, dim:int=2):
     # Ax = b ->
     # (1 x1) * (c1) = (y1) 
     # (1 x2) * (c2)   (y2)
-    # print(points); exit()
-    AT = np.array([points[0]**i for i in range(dim)])
+
+    AT = matrix([points[0]^i for i in range(dim)])
     A = AT.transpose()
-    ATA = AT @ A
-    coeffs = np.linalg.inv(ATA) @ AT @ points[1]
+    ATA = AT * A
+    coeffs = (ATA.inverse()) * AT * vector(points[1])
     return np.polynomial.Polynomial(coeffs)
 
 def trig_regression_ez(points):
     # f(x) = a + b * sin(x)
     xs = points[:,0]
     ys = points[:,1]
-    AT = np.array([np.ones_like(xs), sin(xs)])
+    AT = matrix([[1, sin(x)] for x in xs])
     A = AT.transpose()
-    ATA = AT @ A
-    a,b = np.linalg.inv(ATA) @ AT @ ys
-    return lambda x: a+b*sin(x)
+    ATA = AT * A
+    a, b = (ATA.inverse()) * AT * vector(ys)
+    return lambda x: a + b * sin(x)
 
 def trig_regression_med(points):
     # f(x) = a * sin(wx) + b * cos(ox)
     w, o = 1.2, -2.3
-    xs = points[:,0]
-    ys = points[:,1]
-    AT = np.array([sin(w * xs), cos(o * xs)])
+    xs = [point[0] for point in points]
+    ys = [point[1] for point in points]
+    AT = matrix([[sin(w * x), cos(o * x)] for x in xs])
     A = AT.transpose()
-    ATA = AT @ A
-    a,b = np.linalg.inv(ATA) @ AT @ ys
-    return lambda x: a+b*sin(x)
+    ATA = AT * A
+    a, b = (ATA.inverse()) * AT * vector(ys)
+    return lambda x: a + b * sin(x)
